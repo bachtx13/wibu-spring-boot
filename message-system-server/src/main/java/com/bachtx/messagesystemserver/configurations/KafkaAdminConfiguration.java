437c7d18e0df6b8a451ad1350a859147c8cbe9eb
bachtx13 <bachtran13122003@gmail.com>
2024-07-23 19:29:38 +0700

    update docker compose

--- message-system-server/src/main/java/com/bachtx/messagesystemserver/configurations/KafkaAdminConfiguration.java
+++ message-system-server/src/main/java/com/bachtx/messagesystemserver/configurations/KafkaAdminConfiguration.java
@@ -2,6 +2,7 @@ package com.bachtx.messagesystemserver.configurations;
 
 import org.apache.kafka.clients.admin.AdminClientConfig;
 import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.KafkaAdmin;
@@ -10,10 +11,12 @@ import java.util.Map;
 
 @Configuration
 public class KafkaAdminConfiguration {
+    @Value("${spring.kafka.bootstrap-servers: localhost:9092}")
+    private String bootstrapServers;
     @Bean
     public KafkaAdmin kafkaAdmin() {
         return new KafkaAdmin(Map.of(
-                AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092"
+                AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers
         ));
     }
 
